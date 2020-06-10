1c3995336beb7c3f472c2a69f00f0baf94397703
Blaž <blaz.rupnik56@gmail.com>
2020-06-10 13:17:50 +0200

    Added openmp optimization using reduction

---
+++ parallel_openmp_optimized.cpp
@@ -0,0 +1,158 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <cstdlib>
+#include <string.h>
+#include <math.h>
+#include <time.h>
+#include <omp.h>
+#include "FreeImage.h"
+
+void printImage(unsigned char *image, int size){
+    //helper function for debugging purposes
+    for(int i = 0; i < (size); i = i + 4){
+        int image_position = i / 4;
+        printf("index: %d, B: %d, G: %d, R: %d, A: %d \n", image_position, image[i], image[i+1], image[i+2], image[i+3]);
+    }
+}
+
+void printArrayWithStep4(int* arrayToPrint, int size){
+    //helper function for debugging purposes
+    for(int c = 0; c < (size); c = c + 4){
+        printf("%d %d %d %d \n", arrayToPrint[c], arrayToPrint[c+1], arrayToPrint[c+2], arrayToPrint[c+3]);
+    }
+}
+
+void initCentroids(int *centroids, int num_of_clusters, unsigned char* imageIn, int imageSize){
+    int counter = 0;
+    //we use interval to select starting centroids from image, spreaded equally across
+    int interval = imageSize / num_of_clusters;
+    for(int i = 0; i < (num_of_clusters * 4); i = i + 4){
+        centroids[i] = imageIn[counter];
+        centroids[i + 1] = imageIn[counter+1];
+        centroids[i + 2] = imageIn[counter+2];
+        centroids[i + 3] = imageIn[counter+3];
+        counter = counter + (interval * 4);
+    }
+}
+
+void applyNewCentroidValue(int centroidIndex, int* centroids, int* closest_centroid_indices, unsigned char *image, int size){
+    int count = 0;
+    int blue = 0;
+    int green = 0;
+    int red = 0;
+    int alpha = 0;
+    //go through all points and compute average for all belonging to this cluster
+    #pragma omp parallel for reduction(+:count,blue,green,red,alpha)
+    for(int i = 0; i < (size); i++){
+        if(closest_centroid_indices[i] == centroidIndex){
+            count++;
+            blue = blue + image[i * 4];
+            green = green + image[i * 4 + 1];
+            red = red + image[i * 4 + 2];
+            alpha = alpha + image[i * 4 + 3];
+        }
+    }
+    if(count > 0){
+        centroids[centroidIndex * 4] = blue / count;
+        centroids[centroidIndex * 4 + 1] = green / count;
+        centroids[centroidIndex * 4 + 2] = red / count;
+        centroids[centroidIndex * 4 + 3] = alpha / count;
+    }
+}
+
+int findClosestCentroid(int *centroids, int num_of_clusters, int blue, int green, int red, int alpha){
+    //we init index and distance to 1st centroid, than compute for the remaining ones
+    int centroidIndex = 0;
+    float minimum_distance = sqrt(pow((float)(centroids[0] - blue), 2.0) + pow((float)(centroids[1] - green), 2.0) + pow((float)(centroids[2] - red), 2.0)
+        + pow((float)(centroids[3] - alpha), 2.0));
+
+    for(int i = 4; i < (num_of_clusters * 4); i = i + 4){
+        float current_distance = sqrt(pow((float)(centroids[i] - blue), 2.0) + pow((float)(centroids[i+1] - green), 2.0) + pow((float)(centroids[i+2] - red), 2.0)
+            + pow((float)(centroids[i+3] - alpha), 2.0));
+        if(current_distance < minimum_distance){
+            centroidIndex = i / 4;
+            minimum_distance = current_distance;
+        }
+    }
+    return centroidIndex;
+}
+
+void applyNewColoursToImage(unsigned char* image, int* closest_centroid_indices,  int size, int num_of_clusters, int* centroids){
+    //for each pixel in image assign it new centroid colour
+    for(int i = 0; i < (size); i = i + 4){
+        //find colour centroid for this pixel
+        int closestCentroid = closest_centroid_indices[i/4];
+        //apply centroid colour to this pixel
+        image[i] = centroids[closestCentroid * 4];
+        image[i+1] = centroids[closestCentroid * 4 + 1];
+        image[i+2] = centroids[closestCentroid * 4 + 2];
+        image[i+3] = centroids[closestCentroid * 4 + 3];
+    }
+}
+
+int main(int argc, char *argv[]){
+    //Load image from file
+    //1st argument is image name including format
+	FIBITMAP *imageLoad = FreeImage_Load(FIF_PNG, argv[1], 0);
+	//Convert it to a 32-bit image
+    FIBITMAP *imageLoad32 = FreeImage_ConvertTo32Bits(imageLoad);
+	
+    //Get image dimensions
+    int width = FreeImage_GetWidth(imageLoad32);
+	int height = FreeImage_GetHeight(imageLoad32);
+	int pitch = FreeImage_GetPitch(imageLoad32);
+
+	//Prepare room for a raw data copy of the image
+    unsigned char *imageIn = (unsigned char *)malloc(height*pitch * sizeof(unsigned char));
+
+    //Extract raw data from the image
+	FreeImage_ConvertToRawBits(imageIn, imageLoad32, pitch, 32, FI_RGBA_RED_MASK, FI_RGBA_GREEN_MASK, FI_RGBA_BLUE_MASK, TRUE);
+
+    //Free source image data free
+	FreeImage_Unload(imageLoad32);
+	FreeImage_Unload(imageLoad);
+
+    //get number of clusters from 2nd argument and num of iterations from 3rd argument
+    int num_of_clusters = atoi(argv[2]);
+    int num_of_iterations = atoi(argv[3]); 
+
+    //centroid init array
+    int *centroids = (int*)malloc(num_of_clusters * 4 * sizeof(int));
+    initCentroids(centroids, num_of_clusters, imageIn, width * height);
+
+    //init array for keeping indices of closest centroid
+    int *closest_centroid_indices = (int*)malloc(width * height * sizeof(int));
+
+    for(int iteration = 0; iteration < (num_of_iterations); iteration++){
+        double iteration_start = omp_get_wtime();
+        //step 1: go through all points and find closest centroid
+        #pragma omp parallel for
+        for(int point = 0; point < (width*height); point++){
+            int imageStartingPointIndex = point * 4;
+            int blue = imageIn[imageStartingPointIndex];
+            int green = imageIn[imageStartingPointIndex + 1];
+            int red = imageIn[imageStartingPointIndex + 2];
+            int alpha = imageIn[imageStartingPointIndex + 3];
+            closest_centroid_indices[point] = findClosestCentroid(centroids, num_of_clusters, blue, green, red, alpha);
+        }
+        //step 2: for each centroid compute average which will be new centroid        
+        for(int centroid = 0; centroid < (num_of_clusters); centroid++){
+            applyNewCentroidValue(centroid, centroids, closest_centroid_indices, imageIn, width*height);
+        }
+        double iteration_end = omp_get_wtime();
+        if(iteration == 0){
+            printf("%f\n", iteration_end - iteration_start);
+        }
+    }
+
+    //apply new colours to input image
+    //applyNewColoursToImage(imageIn, closest_centroid_indices, pitch*height, num_of_clusters, centroids);
+
+    //printf("IMAGE: \n");
+    //printImage(imageIn, width * height);
+
+    // Save image
+	//FIBITMAP *imageOutBitmap = FreeImage_ConvertFromRawBits(imageIn, width, height, pitch, 32, FI_RGBA_RED_MASK, FI_RGBA_GREEN_MASK, FI_RGBA_BLUE_MASK, TRUE);
+	//FreeImage_Save(FIF_PNG, imageOutBitmap, "output/test_openmp_second.png", 0);
+	//FreeImage_Unload(imageOutBitmap);
+}
